b26381e8ec47c1928ccac489166a7a35e1b4b95e
m0rp3x <67301219+m0rp3x@users.noreply.github.com>
2022-06-27 20:52:28 +0300

    ВАЙФУ ИДИ НАХУЙ

--- main.py
+++ main.py
@@ -1,51 +1,39 @@
 import asyncio
-from asyncio import sleep
-from random import randint
 
 import discord
 import pymysql as pymysql
-from discord import channel
 from discord.ext import commands
+
 from pymysql.cursors import DictCursor
 
 from config import settings
 
 dbh = pymysql.connect(
-        host='абоба',
-        port=абоба
-        user='абоба
-        password='абоба
-        db='абоба
-        charset='utf8mb4',
-        cursorclass=DictCursor,
-        autocommit=True
-    )
+    host='109.120.190.242',
+    port=3306,
+    user='root',
+    password='qBND9KgPur9jAxDwcRrEmReN7y2HBwWFYTYc',
+    db='datebaseass',
+    charset='utf8mb4',
+    cursorclass=DictCursor,
+    autocommit=True
+)
 cur = dbh.cursor()
 
 bot = commands.Bot(command_prefix=settings['prefix'])
 
 
-
-
-
-
- guild = bot.get_guild(498237888782401567)
-    category = discord.utils.get(guild.categories, name='AUTH')
-    overwrites = {
-
-    }
-    global channel
-    channel = await guild.create_text_channel(f'{ctx.message.author}', category=category, overwrites=overwrites)
+@bot.command(pass_context=True)
+async def auth(ctx, email, ):
     member = ctx.message.author
-
     cur.execute(f"""SELECT Email, Tier, status, Name FROM datebase_with_cute_ass WHERE Email = '{email}';""")
     aboba = cur.fetchone()
     cur.execute(f"""SELECT Email, Tier, status, Name FROM for_discord_bot WHERE Email = '{email}';""")
     aboba1 = cur.fetchone()
     if aboba == None:
         await ctx.send(f"Such mail does not exist in our database. Are you sure you entered it correctly?")
-        await asyncio.sleep(10)
-        await channel.delete()
+        await ctx.message.delete()
+
     elif not aboba1:
         query = '''INSERT INTO for_discord_bot( Name, Email, Tier, status) VALUES (%s,%s,%s,%s)'''
         values = aboba['Name'], aboba['Email'], aboba['Tier'], '16'
@@ -53,40 +41,45 @@ bot = commands.Bot(command_prefix=settings['prefix'])
         await ctx.send(f"Congratulations! You ha    ve your role!")
         if aboba['Tier'] == 'Supporter':
             role_1 = member.guild.get_role(626123834919092244)
-            await member.add_roles(role_1)
-            await asyncio.sleep(10)
-            await channel.delete()
+            await ctx.message.delete()
+
+
         elif aboba['Tier'] == 'Friend':
             role_3 = member.guild.get_role(626137973918007316)
             await member.add_roles(role_3)
-            await asyncio.sleep(10)
-            await channel.delete()
+            await ctx.message.delete()
+
         elif aboba['Tier'] == 'Best friend':
             role_2 = member.guild.get_role(626138204755591248)
             await member.add_roles(role_2)
-            await asyncio.sleep(10)
-            await channel.delete()
+            await ctx.message.delete()
+
+
         elif aboba['Tier'] == 'Super Best Friend':
             role_2 = member.guild.get_role(671433790639439875)
             await member.add_roles(role_2)
-            await asyncio.sleep(10)
-            await channel.delete()
+            await ctx.message.delete()
+
+
         elif aboba['Tier'] == 'School Game is my life!':
             role_2 = member.guild.get_role(671431724684673025)
             await member.add_roles(role_2)
-            await asyncio.sleep(10)
-            await channel.delete()
+            await ctx.message.delete()
+
+
+
     else:
         await ctx.send(f"A role has already been issued for this email.")
         await asyncio.sleep(10)
-        await channel.delete()
+        await ctx.message.delete()
+        await ctx.send.delete()
 
 
 @bot.command()
 async def create(ctx):
-    guild = bot.get_guild(990986544611491900)
+    guild = bot.get_guild(498237888782401567)
 
-    category = discord.utils.get(guild.categories, name="auth")
+    category = discord.utils.get(guild.categories, name='AUTH')
 
     overwrites = {
 
@@ -95,9 +88,10 @@ async def create(ctx):
     await channel.set_permissions(ctx.guild.default_role,
                                   read_messages=False)
     await channel.set_permissions(ctx.message.author, read_messages=True, send_messages=True)
-
-
-
+    await ctx.message.delete()
+    if channel:
+        await asyncio.sleep(120)
+        await channel.delete()
 
 
 @bot.command()
@@ -110,13 +104,17 @@ async def clear(ctx, amount=2):
     await ctx.channel.purge(limit=amount)
     await ctx.message.delete()
 
+
 @bot.command()
 async def sos(ctx):
-    await ctx.send("To get your role , write !create, and then in the channel created for yourself !auth and your email\n Example: !auth test@outlook.com")
+    await ctx.send(
+        "To get your role , write !create, and then in the channel created for yourself !auth and your email\n Example: !auth test@outlook.com")
+
+
 @bot.command()
 async def помощь(ctx):
-   await ctx.send("Чтобы получить свою роль напиши !create, а после в созданном для себя канале !auth и свою почту\n Пример: !auth test@outlook.com")
-
+    await ctx.send(
+        "Чтобы получить свою роль напиши !create, а после в созданном для себя канале !auth и свою почту\n Пример: !auth test@outlook.com")
 
 
 bot.run(settings['token'])
